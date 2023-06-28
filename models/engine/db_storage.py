e62ae85d9b480c22720bfecec751f9392ee560c2
CynthiaUmuhire <c.umuhire@alustudent.com>
2023-06-28 21:04:49 +0200

    P8P

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,7 +32,8 @@ class DBStorage:
         host = os.getenv('HBNB_MYSQL_HOST')
         database = os.getenv('HBNB_MYSQL_DB')
         user = os.getenv('HBNB_MYSQL_USER')
-        string = 'mysql+mysqldb://{}:{}@{}/{}'.format(user,                                                      password,
+        string = 'mysql+mysqldb://{}:{}@{}/{}'.format(user,
+                                                      password,
                                                       host,
                                                       database)
         self.__engine = sqlalchemy.create_engine(string, pool_pre_ping=True)
